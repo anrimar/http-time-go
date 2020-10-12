d82a7e6c5df41d276b1d4178b647442ef4b642e5
Goga Eva <igorevtushenko22@gmail.com>
2020-10-12 18:55:31 +0300

    GET time

--- server.go
+++ server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"time"
 	"net/http"
+	"encoding/json"
 )
 
+type info struct {
+	t string
+}
+
+func timeHandler (w http.ResponseWriter, r *http.Request) {
+	//resp := info{t: time.Now().Format(time.RFC3339)}
+	t := time.Now().Format(time.RFC3339)
+	json.NewEncoder(w).Encode(t)
+}
+
 func main() {
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		//fmt.Fprintf(w, "Hello World!")
-	})
+	http.HandleFunc("/time", timeHandler)
 	http.ListenAndServe(":8795", nil)
 }
