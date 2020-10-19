41459382f2d0e6c6ef9a39caa91fea5b2cce39b5
Goga Eva <igorevtushenko22@gmail.com>
2020-10-19 23:35:14 +0300

    comment about handler

--- server.go
+++ server.go
@@ -10,12 +10,12 @@ import (
 type info struct {
 	T string `json:"time"`
 }
-
+//This function will be executed when performing an HTTP GET request to /time
 func timeHandler (w http.ResponseWriter, r *http.Request) {
 	resp := info{time.Now().Format(time.RFC3339)}
 	err := json.NewEncoder(w).Encode(resp)
 	if err != nil {
-	log.Println(err)
+		log.Println(err)
 	}
 }
 
